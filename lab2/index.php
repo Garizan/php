d6ed61b78e25d680a7c16d99d5f92865a176fb1d
Fiodor Garizan <fedost601@gmail.com>
2026-02-15 21:57:33 +0200

    Изменил lab2

--- lab2/index.php
+++ lab2/index.php
@@ -1,4 +1,16 @@
 <?php 
 
-echo "Hello world!"
+echo "Hello, World!<br />";
+
+echo "Hello, World with echo!<br />";
+
+print "Hello, World with print<br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "{$days} {$message}<br />";
+
+echo $days . " " . $message;
+
 ?>
